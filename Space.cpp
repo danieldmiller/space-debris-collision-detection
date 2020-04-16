783b27498a0883f6203968ee3ce6a181288f7a10
Paul Laffitte <paul.laffitte@epitech.eu>
2020-04-16 17:27:14 +0700

    Replaced debris array by std::vector
    This allows to improve performances and makes the code easier to understand

--- Space.cpp
+++ Space.cpp
@@ -7,7 +7,7 @@ double Space::time = 0.0;
 Space::Space(int amountOfDebris0)
 {
 	amountOfDebris = amountOfDebris0;
-	debris = new SpaceObject[amountOfDebris];
+	// debris = new SpaceObject[amountOfDebris];
 
 	//initialize the limits for the space
 	//for(int i = 0; i < 8 ; i++){
@@ -17,13 +17,11 @@ Space::Space(int amountOfDebris0)
 
 Space::Space(const Space& rf) {
 	amountOfDebris = rf.amountOfDebris;
-	debris = new SpaceObject[amountOfDebris];
-	*debris = *rf.debris;
+	debris = rf.debris;
 }
 
 Space::~Space()
 {
-	delete[] debris;
 }
 
 void Space::initializeObjects()
@@ -34,13 +32,13 @@ void Space::initializeObjects()
     std::default_random_engine re;
     double a_random_double = uniform(re);
 
-    for(int i=0; i<amountOfDebris; i++) {
+    for (int i = 0; i < amountOfDebris; i++) {
         double mass = uniform(re);
         double radius = uniform(re);
         double x = uniform(re);
         double y = uniform(re);
         double z = uniform(re);
-        *(debris + i) = SpaceObject(mass, radius, x, y, z);
+        debris.emplace_back(mass, radius, x, y, z);
     }
 }
 
@@ -48,25 +46,23 @@ void Space::updateObjects()
 {
 
     for (int i = 0; i < amountOfDebris; i++) {
+        SpaceObject &obj_i = debris[i];
         for(int j = i + 1; j < amountOfDebris; j++) {
-            SpaceObject obj_i = *(debris + i);
-            SpaceObject obj_j = *(debris + j);
+            SpaceObject &obj_j = debris[j];
             obj_i.getGravitationalForceX(obj_j);
             obj_i.getGravitationalForceY(obj_j);
             obj_i.getGravitationalForceZ(obj_j);
-            *(debris + i) = obj_i;
         }
    }
 
     for (int i = 0; i < amountOfDebris; i++) {
-        SpaceObject obj = *(debris + i);
+        SpaceObject &obj = debris[i];
         obj.getAllGravitationalForces(time);
         obj.recordDirection();
-        *(debris + i) = obj;
     }
 
     for (int i = 0; i < amountOfDebris; i++) {
-        (debris + i)->recordNewLocation((debris + i)->returnPoint());
+        debris[i].recordNewLocation(debris[i].returnPoint());
     }
 
     time = time + 0.000001; // add time in nanoseconds
